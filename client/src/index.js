5cc92e8ee21cffbe92bf13bf32e611ee2f5e0712
Jorge Leandro Olaizola <78763921+JorgeOlaizola@users.noreply.github.com>
2021-07-22 22:01:08 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import dotenv from 'dotenv'
+import axios from 'axios'
 
 dotenv.config()
 
